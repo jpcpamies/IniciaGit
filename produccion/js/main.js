71ed976354cc42c3eb0bdfbba3d37742232465e6
Jordi Pàmies <jordipamies2@gmail.com>
2013-11-06 02:22:47 +0000

    Añadido Disqus para los comentarios

--- produccion/js/main.js
+++ produccion/js/main.js
@@ -1,4 +1,28 @@
 
+// Disqus   
+var disqus_shortname = 'iniciagit'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+    
+// Disqus complemento
+var disqus_shortname = 'iniciagit'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+
+    
+
+
 // Plugin scrollTop
 $(function(){
 	//clic en un enlace de la lista
