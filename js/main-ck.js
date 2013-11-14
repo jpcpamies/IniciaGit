f0ca6200ad8ebf64cf3d5170b2a29e92b241248f
Jordi Pàmies <jordipamies2@gmail.com>
2013-11-14 13:23:42 +0000

    Estilado header y nav movile first

---
+++ js/main-ck.js
@@ -0,0 +1,2 @@
+// Script mobileMenu
+$(function(){$("#mobileMenu").mobileMenu({combine:!1,groupPageText:"",nested:!0,prependTo:"nav",switchWidth:992,topOptionText:""})});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+// Script mobileMenu
+$(function(){
+$('#mobileMenu').mobileMenu({
+  combine: false,         
+  groupPageText: '',     
+  nested: true,        
+  prependTo: 'nav',      
+  switchWidth: 992,     
+  topOptionText: ''
+});
+})
