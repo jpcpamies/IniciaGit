5f43dc91c720f9c9e81e014e667021e9a86137df
Jordi Pàmies <jordipamies2@gmail.com>
2013-11-15 00:35:39 +0000

    Nav fixed al scroll

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,2 +1,2 @@
 // Script mobileMenu
-$(function(){$("#mobileMenu").mobileMenu({combine:!1,groupPageText:"",nested:!0,prependTo:"nav",switchWidth:992,topOptionText:""})});
+$(function(){$("#mobileMenu").mobileMenu({combine:!1,groupPageText:"",nested:!0,prependTo:"nav",switchWidth:992,topOptionText:""})});var num=180;$(window).bind("scroll",function(){$(window).scrollTop()>num?$(".menu").addClass("fixed"):$(".menu").removeClass("fixed")});
--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ $('#mobileMenu').mobileMenu({
   topOptionText: ''
 });
 })
+
+// Script nav fixed
+  var num = 180; //number of pixels before modifying styles
+
+  $(window).bind('scroll', function () {
+      if ($(window).scrollTop() > num) {
+          $('.menu').addClass('fixed');
+      } else {
+          $('.menu').removeClass('fixed');
+      }
+  });
