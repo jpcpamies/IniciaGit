9f7df0e2b05b4411c8de0fae915a9326372092f9
Jordi Pàmies <jordipamies2@gmail.com>
2013-11-24 21:22:27 +0000

    Añadido hero efecto parallax
    hero fijo en movil

--- js/main.js
+++ js/main.js
@@ -27,10 +27,10 @@ var num = 180; //number of pixels before modifying styles
 		});
 
 
-// Script nav fixed =============================================================
+// Script nav fixed movil =============================================================
 // ==============================================================================
 
-  var num2 = 180; //number of pixels before modifying styles
+  var num2 = 185; //number of pixels before modifying styles
 
   $(window).bind('scroll', function () {
       if ($(window).scrollTop() > num2) {
@@ -39,3 +39,19 @@ var num = 180; //number of pixels before modifying styles
           $('.menu').removeClass('fixed');
       }
   });
+
+
+// Script nav fixed escitorio =============================================================
+// ==============================================================================
+
+  var num3 = 120; //number of pixels before modifying styles
+
+  $(window).bind('scroll', function () {
+      if ($(window).scrollTop() > num3) {
+          $('.tope').addClass('fijo');
+      } else {
+          $('.tope').removeClass('fijo');
+      }
+  });
+
+
